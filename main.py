fa07db1f4bfeb8a7a2010111952e7cb49c52eb28
bellkelsin225-dotcom <bellkelsin225@gmail.com>
2026-02-28 12:23:14 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))  # Convertir la entrada a entero
+
+edad_el_proximo_ano = edad + 1
+
+print("Hola,", nombre, "¡en un año tendrás", edad_el_proximo_ano, "años!")
